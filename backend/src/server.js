0985211f1fa99cf1faa35c07026af1d6839bd80a
Gabriel Cruz <gabriel.cruz2@hotmail.com>
2019-11-27 22:54:13 -0300

    Alteração do CORS

--- backend/src/server.js
+++ backend/src/server.js
@@ -27,7 +27,14 @@ app.use((req, res, next) => {
 });7
 
 app.use(cors());
-app.options('*', cors());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
 app.use(express.json());
 app.use(rotas);
 
